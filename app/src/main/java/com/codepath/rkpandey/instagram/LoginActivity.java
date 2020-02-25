113186239a7e6044850003f12214412be6eae09c
Sama Manalai <manalais@berea.edu>
2020-02-24 21:55:53 -0500

    loginActivity

--- app/src/main/java/com/codepath/rkpandey/instagram/LoginActivity.java
+++ app/src/main/java/com/codepath/rkpandey/instagram/LoginActivity.java
@@ -1,16 +1,26 @@
 package com.codepath.rkpandey.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG ="LoginActivity";
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
@@ -23,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 login(username, password);
@@ -31,6 +42,24 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login(String username, String password){
-        //Todo: navigate to new activity if the user has signed properly
+        Log.i(TAG, "Attempting to login user" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "issue with login", e);
+                    return;
+                }
+                //Todo: navigate to new activity if the user has signed properly
+                gotMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void gotMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/codepath/rkpandey/instagram/ParseApplication.java
+++ app/src/main/java/com/codepath/rkpandey/instagram/ParseApplication.java
@@ -2,5 +2,20 @@ package com.codepath.rkpandey.instagram;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("sama-instagram") // should correspond to APP_ID env variable
+                .clientKey("SamaManalaiCodePath")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://sama-instagram.herokuapp.com/parse").build());
+
+    }
 }
