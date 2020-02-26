8962debb566c80a263a8123a8418e9a7285da2c7
Sama Manalai <manalais@berea.edu>
2020-02-25 22:47:17 -0500

    loginActivity

--- app/src/main/java/com/codepath/rkpandey/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/rkpandey/instagram/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCaptureImage;
     private ImageView ivPostImage;
     private Button btnSubmit;
+    private Button btnLogout;
     private File photoFile;
     public String photoFileName = "photo.jpg";
 
@@ -48,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage = findViewById(R.id.btnCaptureImage);
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
+        btnLogout = findViewById(R.id.btnLogout);
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goLogoutActivity();
+            }
+
+        });
 
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -148,6 +162,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void goLogoutActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 
     private void queryPost() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
